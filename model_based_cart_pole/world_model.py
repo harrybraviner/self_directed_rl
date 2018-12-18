9a443bdd6ae6d0873daa3de79cdb5143e4edbda5
Harry J. Braviner <harry.braviner@gmail.com>
2018-12-18 23:55:44 +0000

    Now models the reward and done as well as state.

--- model_based_cart_pole/world_model.py
+++ model_based_cart_pole/world_model.py
@@ -33,10 +33,20 @@ class WorldModel:
         hidden_state_2 = slim.fully_connected(hidden_state, hidden_size, biases_initializer=None, activation_fn=tf.nn.relu)
 
         self.state_output = slim.fully_connected(hidden_state_2, state_space_size, biases_initializer=None, activation_fn=None)
+        self.reward_output = slim.fully_connected(hidden_state_2, 1, biases_initializer=None, activation_fn=None)
+        self.done_output = slim.fully_connected(hidden_state_2, 1, biases_initializer=None, activation_fn=tf.nn.sigmoid)
+
 
         self.state_output_ground_truth = tf.placeholder(shape=[None, state_space_size], dtype=tf.float32)
+        self.reward_ground_truth = tf.placeholder(shape = [None, 1], dtype=tf.float32)
+        self.done_ground_truth = tf.placeholder(shape = [None, 1], dtype=tf.float32)
 
-        self.loss = tf.losses.mean_squared_error(self.state_output_ground_truth, self.state_output)
+        self.state_loss = tf.losses.mean_squared_error(self.state_output_ground_truth, self.state_output)
+        self.reward_loss = tf.losses.mean_squared_error(self.reward_ground_truth, self.reward_output)
+        # FIXME - should REALLY make this a classification loss
+        self.done_loss = tf.losses.mean_squared_error(self.done_ground_truth, self.done_output)
+
+        self.loss = self.state_loss + self.reward_loss + self.done_loss
 
         self.learning_rate = tf.placeholder(shape=[], dtype=tf.float32)
         optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate)
@@ -49,18 +59,21 @@ class WorldModel:
 
         return EnvAnalogue(sess, state_stepper, state_initializer)
 
-    def train_on_episodes(self, state_input, action_input, state_output, learning_rate, sess):
+    def train_on_episodes(self, state_input, action_input, state_output, reward, done, learning_rate, sess):
         feed_dict={
             self.state_input: state_input,
             self.action_input: action_input,
             self.state_output_ground_truth: state_output,
+            self.reward_ground_truth: reward,
+            self.done_ground_truth: done,
             self.learning_rate: learning_rate
         }
 
         # FIXME - how should I get the session in here?
         # Should this function actually return a step?
-        _, training_loss = sess.run([self.train_step, self.loss], feed_dict=feed_dict)
-        return training_loss
+        _, training_loss, state_loss, reward_loss, done_loss =\
+            sess.run([self.train_step, self.loss, self.state_loss, self.reward_loss, self.done_loss], feed_dict=feed_dict)
+        return training_loss, state_loss, reward_loss, done_loss
 
 
 if __name__ == "__main__":
@@ -78,21 +91,25 @@ if __name__ == "__main__":
             world_model.action_input: np.array([1])
         }
 
-        output = sess.run(world_model.state_output, feed_dict=feed_dict)
+        output = sess.run([world_model.state_output, world_model.reward_output, world_model.done_output], feed_dict=feed_dict)
         print(output)
 
         world_model.train_on_episodes(state_input=np.array([[0.1, -0.4, 1.3, -0.1]]), action_input=np.array([1]),
-                                      state_output=np.array([[0.4, -0.2, 1.5, 0.0]]), learning_rate=1e-2, sess=sess)
+                                      state_output=np.array([[0.4, -0.2, 1.5, 0.0]]),
+                                      reward=np.array([[0.5]]), done=np.array([[1.0]]), learning_rate=1e-2, sess=sess)
     print("Passed a single training step without crashing.\n")
 
     # Show that we can train the model
     import gym
+    from ewma import EWMA
     tf.reset_default_graph()
 
     def make_episode(env, policy, max_length=None):
         states_in = []
         states_out = []
         actions = []
+        rewards = []
+        dones = []
 
         s = env.reset()
         done = False
@@ -104,14 +121,18 @@ if __name__ == "__main__":
             states_in.append(s)
             states_out.append(s1)
             actions.append(a)
+            rewards.append([reward])
+            dones.append([1.0 if done else 0.0])
 
             s = s1
 
         states_in = np.stack(states_in, axis=0)
         states_out = np.stack(states_out, axis=0)
         actions = np.stack(actions, axis=0)
+        rewards = np.stack(rewards, axis=0)
+        dones = np.stack(dones, axis=0)
 
-        return states_in, states_out, actions
+        return states_in, states_out, actions, rewards, dones
 
     env = gym.make("CartPole-v0")
     action_space_size = env.action_space.n
@@ -128,21 +149,24 @@ if __name__ == "__main__":
         policy = lambda s : rng.choice(2)
 
         smoothing_gamma = 0.01
-        smoothed_loss = None
+        smoothed_state_loss = EWMA(0.99)
+        smoothed_reward_loss = EWMA(0.99)
+        smoothed_done_loss = EWMA(0.99)
 
         for batch in range(3001):
-            states_in, states_out, actions = make_episode(env, policy)
+            states_in, states_out, actions, rewards, dones = make_episode(env, policy)
 
-            loss = world_model.train_on_episodes(states_in, actions, states_out, learning_rate=1e-2, sess=sess)
+            _, state_loss, reward_loss, done_loss =\
+                    world_model.train_on_episodes(states_in, actions, states_out, rewards, dones, learning_rate=1e-2, sess=sess)
 
-            if smoothed_loss is None:
-                smoothed_loss = loss
-            else:
-                smoothed_loss *= (1.0 - smoothing_gamma)
-                smoothed_loss += smoothing_gamma * loss
+            smoothed_state_loss.update(state_loss)
+            smoothed_reward_loss.update(reward_loss)
+            smoothed_done_loss.update(done_loss)
 
             if batch % 500 == 0:
-                print("Episode: {}\tSmoothed loss: {}".format(batch, smoothed_loss))
+                print("Episode: {}\tSmoothed losses: State: {}\tReward: {}\tDone: {}".format(batch, smoothed_state_loss.value(),
+                                                                                            smoothed_reward_loss.value(),
+                                                                                            smoothed_done_loss.value()))
 
         print("Typically the final training loss should be < 0.01.")
 
