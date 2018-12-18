17509aa46268547af810d8f60c97d69efc1cc337
Harry J. Braviner <harry.braviner@gmail.com>
2018-12-18 16:48:34 +0000

    Added training test to world_model

--- model_based_cart_pole/world_model.py
+++ model_based_cart_pole/world_model.py
@@ -59,3 +59,66 @@ if __name__ == "__main__":
 
         world_model.train_on_episodes(state_input=np.array([[0.1, -0.4, 1.3, -0.1]]), action_input=np.array([1]),
                                       state_output=np.array([[0.4, -0.2, 1.5, 0.0]]), sess=sess)
+    print("Passed a single training step without crashing.\n")
+
+    # Show that we can train the model
+    import gym
+    tf.reset_default_graph()
+
+    def make_episode(env, policy, max_length=None):
+        states_in = []
+        states_out = []
+        actions = []
+
+        s = env.reset()
+        done = False
+
+        while (not done) and (max_length is None or length < max_length):
+            a = policy(s)
+            s1, reward, done, _ = env.step(a)
+
+            states_in.append(s)
+            states_out.append(s1)
+            actions.append(a)
+
+            s = s1
+
+        states_in = np.stack(states_in, axis=0)
+        states_out = np.stack(states_out, axis=0)
+        actions = np.stack(actions, axis=0)
+
+        return states_in, states_out, actions
+
+    env = gym.make("CartPole-v0")
+    action_space_size = env.action_space.n
+    state_space_size = env.observation_space.shape[0]
+
+    print("Attempting training on many episodes (with random choices of action). Loss should decrease.")
+
+    world_model = WorldModel(state_space_size, action_space_size, hidden_size=32)
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+
+        rng = np.random.RandomState(123)
+        policy = lambda s : rng.choice(2)
+
+        smoothing_gamma = 0.01
+        smoothed_loss = None
+
+        for batch in range(3001):
+            states_in, states_out, actions = make_episode(env, policy)
+
+            loss = world_model.train_on_episodes(states_in, actions, states_out, learning_rate=1e-2, sess=sess)
+
+            if smoothed_loss is None:
+                smoothed_loss = loss
+            else:
+                smoothed_loss *= (1.0 - smoothing_gamma)
+                smoothed_loss += smoothing_gamma * loss
+
+            if batch % 500 == 0:
+                print("Episode: {}\tSmoothed loss: {}".format(batch, smoothed_loss))
+
+        print("Typically the final training loss should be < 0.01.")
+
